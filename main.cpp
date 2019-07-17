fbef0da4bdac469d954b792dc0d48950a574acfc
Time-Coder <binghuiwang5@gmail.com>
2019-07-18 01:05:57 +0800

    Correct images file path.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-	Image image1("specular.tif");
-	Image image2("sea1.jpg");
-	Image image3("sea2.jpg");
+	Image image1("images/specular.tif");
+	Image image2("images/sea1.jpg");
+	Image image3("images/sea2.jpg");
 	image1.show();
 	image2.show();
 	image3.show();
